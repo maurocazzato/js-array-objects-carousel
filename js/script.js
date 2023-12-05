95f9917cdd9e93ac973d48e6e660f81abdd320d4
Maurocazzato <maurizzio.125@gmail.com>
2023-12-05 10:02:34 +0100

    pulizia spazi

--- js/script.js
+++ js/script.js
@@ -8,24 +8,6 @@
 // Al click dell’utente sulle frecce (sia Prev che Next), il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
 
 
-// const imageArray = [ 
-//     { image: 'img/01.webp', title: 'Marvel\'s Spiderman Miles Morale', text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.', },
-
-//     { image: 'img/02.webp', title: 'Ratchet & Clank: Rift Apart', text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.', },
-
-//     { image: 'img/03.webp', title: 'Fortnite', text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.", },
-
-//     { image: 'img/04.webp', title: 'Stray', text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city', },
-
-//     { image: 'img/05.webp', title: "Marvel's Avengers", text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.', } 
-// ];
-
-// let itemContent = '';
-
-// const images = imageArray.map(item => item.image);
-// const titles = imageArray.map(item => item.title);
-// const texts = imageArray.map(item => item.text);
-
 const imageArray = [ 
     { image: 'img/01.webp', title: 'Marvel\'s Spiderman Miles Morales', text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.', },
 
@@ -37,6 +19,7 @@ const imageArray = [
 
     { image: 'img/05.webp', title: "Marvel's Avengers", text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.', } 
 ];
+
 let itemContent = '';
 
 // creo div con immagini, titolo e testo allegate
